8e0bc46888e43e26a5bca2f90cfe7e2d62072ea9
nishant-dongare <nishantdongare@outlook.com>
2023-07-17 23:33:02 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/services/food/food.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FoodService {
+  constructor() {}
+
+  getAll(): string[] {
+    return [
+      'assets/food1.jpg',
+      'assets/food2.jpg',
+      'assets/food3.jpg',
+      'assets/food4.jpg',
+      'assets/food5.jpg',
+      'assets/food6.jpg',
+      'assets/food7.jpg',
+      'assets/food8.jpg',
+    ];
+  }
+}
