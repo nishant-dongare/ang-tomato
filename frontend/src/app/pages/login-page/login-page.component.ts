ee0c1342be91a3f94046038306cd12fb1a42eba0
nishant-dongare <nishantdongare@gmail.com>
2023-10-17 00:47:07 +0530

    login done

--- frontend/src/app/pages/login-page/login-page.component.ts
+++ frontend/src/app/pages/login-page/login-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -8,13 +8,24 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class LoginPageComponent implements OnInit {
   loginForm!: FormGroup;
+  isSubmitted: boolean = false;
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      email: '',
-      password: '',
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
     });
+  }
+
+  get fc() {
+    return this.loginForm.controls;
+  }
+
+  submit() {
+    this.isSubmitted = true;
+    if (this.loginForm.invalid) return;
+    alert(`${this.fc['email'].value},${this.fc['password'].value}`);
   }
 }
