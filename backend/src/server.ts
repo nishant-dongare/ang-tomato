c35bf5aadf04527797582eccf99bc7f07c81a083
nishant-dongare <nishantdongare@gmail.com>
2023-10-17 14:29:00 +0530

    userService

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
-import { sample_foods, sample_tags } from "./data";
+import jwt from "jsonwebtoken";
+import { sample_foods, sample_tags, sample_users } from "./data";
 
 const app = express();
+app.use(express.json());
 app.use(cors({ credentials: true, origin: ["http://localhost:4200"] }));
 
 app.get("/", (req, res) => {
@@ -38,6 +40,28 @@ app.get("/api/foods/:foodId", (req, res) => {
   const food = sample_foods.find((food: any) => food.id === foodId);
   res.send(food);
 });
+
+app.post("api/user/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = sample_users.find(
+    (user) => user.email === email && user.password === password
+  );
+  if (user) {
+    res.send(generateTokenResponse(user));
+  } else {
+    res.status(400).send("Username or Password is not valid");
+  }
+});
+
+const generateTokenResponse = (user: any) => {
+  const token = jwt.sign(
+    { email: user.email, isAdmin: user.isAdmin },
+    "SomeRandomText",
+    { expiresIn: "30d" }
+  );
+  user.token = token;
+  return user;
+};
 
 const port = 5000;
 
