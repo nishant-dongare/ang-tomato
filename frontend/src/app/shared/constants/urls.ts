44e10e8ae4b9df37e3910194883ada35714b6443
nishant-dongare <nishantdongare@gmail.com>
2024-03-09 00:59:20 +0530

    fixed

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,7 +1,7 @@
 const BASE_URL = 'http://localhost:8080';
 export const FOODS_URL = BASE_URL + '/foods';
 export const FOODS_TAGS_URL = BASE_URL + '/tag';
-export const FOODS_BY_SEARCH_URL = FOODS_URL + '/search/';
+export const FOODS_BY_SEARCH_URL = BASE_URL + '/search/';
 export const FOODS_BY_TAG_URL = BASE_URL + '/tag/';
 export const FOODS_BY_ID_URL = FOODS_URL + '/';
 
